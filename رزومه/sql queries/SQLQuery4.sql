117fb45f8b15cd9492db69ffa94c628dd05da1a6
amirhoseinmirkhalili <prosoftamir@gmail.com>
2026-07-09 16:49:49 +0330

    Delete رزومه directory

--- رزومه/sql queries/SQLQuery4.sql
+++
@@ -1,9 +0,0 @@
-DECLARE @sql NVARCHAR(MAX) = '';
-
-SELECT @sql = @sql + 
-    'UPDATE dbo.[labour_force_all] SET ' + QUOTENAME(COLUMN_NAME) + ' = NULL 
-     WHERE ' + QUOTENAME(COLUMN_NAME) + ' = '''' OR ' + QUOTENAME(COLUMN_NAME) + ' = ''--''; '
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'labour_force_all';
-
-EXEC sp_executesql @sql;
--- رزومه/sql queries/پیدا کردن مقادیر نول در ستون وضعیت فعالیت sql.sql
+++
@@ -1,2 +0,0 @@
-select * from dbo.labour_force_all
-where ActivityStatus is null
